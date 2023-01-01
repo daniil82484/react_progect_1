93f1c0e28aa3b491c2a8f15314da024d4ed974a6
danii <daniil82484@gmail.com>
2023-01-01 17:46:30 +0500

    1

---
+++ client/src/component/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import styles from "./MyPosts.module.css"
+
+const MyPosts = (props) => {
+    return (
+        <div>
+            <h1>My Posts</h1>
+        </div>
+    );
+}
+
+export default MyPosts;
